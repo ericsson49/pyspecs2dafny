e8e5c2335420609efd71717f01a827c2928262b3
Alex Vlasov <avv49@mail.ru>
2022-12-21 01:38:28 +0400

    Create test0000.py
    some initial primitive test

---
+++ tests/test0000.py
@@ -0,0 +1,12 @@
+def test() -> None:
+    a = 1
+    assert a == 1
+    b = True
+    assert b
+    assert b == True
+    c = False
+    assert not c
+    assert c == False
+    d = 1 + 4
+    assert d == 5
+    
