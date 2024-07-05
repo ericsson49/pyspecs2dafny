362fb51002a2842c2e21cd89668a63ea94ad1b2b
Alex Vlasov <avv49@mail.ru>
2024-07-06 03:02:43 +0400

    Refactoring code

--- pynotole/pynotole/simplify_ast.py
+++ pynotole/pynotole/simplify_ast.py
@@ -87,6 +87,9 @@ class AstRewriting(Rewriting):
 
 
 def simplify_ast_rule(fvs, s):
+    def is_simple_value(e: ast.expr) -> bool:
+        return isinstance(e, (ast.Constant, ast.Name, ast.Lambda))
+
     def is_func_like(s):
         return isinstance(s, (ast.Call, ast.BinOp, ast.UnaryOp, ast.Attribute, ast.Subscript))
 
@@ -139,7 +142,7 @@ def simplify_ast_rule(fvs, s):
         assns = []
 
         def process_arg(arg):
-            if not isinstance(arg, ast.Name):
+            if not is_simple_value(arg):
                 fn = fvs.fresh()
                 assns.append(ast.Assign([ast.Name(fn, ast.Store())], arg))
                 return ast.Name(fn, ast.Load())
@@ -162,11 +165,11 @@ def simplify_ast_rule(fvs, s):
 
     def convert_compare_op(e: ast.Compare):
         match e:
-            case ast.Compare(left, ops, operands) if not isinstance(left, ast.Name):
+            case ast.Compare(left, ops, operands) if not is_simple_value(left):
                 fn = fvs.fresh()
                 assns = [ast.Assign([ast.Name(fn, ast.Store())], left)]
                 return assns, ast.Compare(ast.Name(fn, ast.Load), ops, operands)
-            case ast.Compare(ast.Name() as left, ops, [operand, *rest]) if not isinstance(operand, ast.Name):
+            case ast.Compare(ast.Name() as left, ops, [operand, *rest]) if not is_simple_value(operand):
                 fn = fvs.fresh()
                 assns = [ast.Assign([ast.Name(fn, ast.Store())], operand)]
                 return assns, ast.Compare(left, ops, [ast.Name(fn, ast.Load), *rest])
@@ -177,56 +180,75 @@ def simplify_ast_rule(fvs, s):
             case _:
                 assert False
 
+    def is_complex_rvalue(e: ast.expr) -> bool:
+        return (is_func_like(e) and any(not is_simple_value(arg) for arg in extract_func_like_args(e))) \
+                or isinstance(e, ast.BoolOp) \
+                or (isinstance(e, ast.Compare) and
+                    (not is_simple_value(e.left)
+                     or (len(e.comparators) > 0 and not is_simple_value(e.comparators[0]))))
+
+    def convert_rvalue(e: ast.expr):
+        assert is_complex_rvalue(e)
+        if is_func_like(e):
+            return convert_func_like(e)
+        elif isinstance(e, ast.BoolOp):
+            return [], covert_bool_op(e)
+        elif isinstance(e, ast.Compare):
+            return convert_compare_op(e)
+        else:
+            assert False
+
     match s:
-        case ast.Assign([tgt], value) if not isinstance(tgt, ast.Name) and not isinstance(value, ast.Name):
+        case ast.Assign([tgt], value) if not isinstance(tgt, ast.Name) and not is_simple_value(value):
             fn = fvs.fresh()
             return [
                 ast.Assign([ast.Name(fn,ast.Store())], value),
                 ast.Assign([tgt], ast.Name(fn, ast.Load()))
             ]
-        case ast.Assign([ast.Name() as tgt], value) if is_func_like(value) \
-                    and any(not isinstance(arg, ast.Name) for arg in extract_func_like_args(value)):
-            assns, value_ = convert_func_like(value)
+        case ast.Assign([ast.Name() as tgt], value) if is_complex_rvalue(value):
+            assns, value_ = convert_rvalue(value)
             return assns + [ast.Assign([tgt], value_)]
         case ast.Assign([ast.Name() as tgt], ast.IfExp(test, body, orelse)):
             return ast.If(test,
                           [ast.Assign([tgt], body)],
                           [ast.Assign([tgt], orelse)])
-        case ast.Assign([ast.Name() as tgt], ast.BoolOp() as value):
-            return ast.Assign([tgt], covert_bool_op(value))
-        case ast.Assign([ast.Name() as tgt], ast.Compare(left, _, [operand, *_]) as value) \
-                if any(not isinstance(v, ast.Name) for v in [left, operand]):
-            assns, value_ = convert_compare_op(value)
-            return assns + [ast.Assign([tgt], value_)]
-        case ast.Assign([ast.Attribute(value, attr)], ast.Name() as v) if not isinstance(value, ast.Name):
+        case ast.Assign([ast.Attribute(value, attr)], ast.Name() as v) if not is_simple_value(value):
             fn = fvs.fresh()
             return [
                 ast.Assign([ast.Name(fn, ast.Store())], value),
                 ast.Assign([ast.Attribute(ast.Name(fn, ast.Load()), attr)], v)
             ]
-        case ast.Assign([ast.Subscript(value, index)], ast.Name() as v) if not isinstance(value, ast.Name):
+        case ast.Assign([ast.Subscript(value, index)], ast.Name() as v) if not is_simple_value(value):
             fn = fvs.fresh()
             return [
                 ast.Assign([ast.Name(fn, ast.Store())], value),
                 ast.Assign([ast.Subscript(ast.Name(fn, ast.Load()), index)], v)
             ]
-        case ast.Assign([ast.Subscript(ast.Name() as value, index)], ast.Name() as v) if not isinstance(index, ast.Name):
+        case ast.Assign([ast.Subscript(ast.Name() as value, index)], ast.Name() as v) if not is_simple_value(index):
             fn = fvs.fresh()
             return [
                 ast.Assign([ast.Name(fn, ast.Store())], index),
                 ast.Assign([ast.Subscript(value, ast.Name(fn, ast.Load()))], v)
             ]
-        case ast.Expr(value) if is_func_like(value) \
-                    and any(map(lambda arg: not isinstance(arg, ast.Name), extract_func_like_args(value))):
-            assns, value_ = convert_func_like(value)
+        case ast.AnnAssign(tgt, anno, value, simple) if is_complex_rvalue(value):
+            assns, value_ = convert_rvalue(value)
+            return assns + [ast.AnnAssign(tgt, anno, value_, simple)]
+        case ast.AnnAssign(tgt, anno, ast.IfExp(test, body, orelse), simple):
+            return ast.If(test,
+                          [ast.AnnAssign(tgt, anno, body, simple)],
+                          [ast.AnnAssign(tgt, anno, orelse, simple)])
+        case ast.Expr(value) if is_complex_rvalue(value):
+            assns, value_ = convert_rvalue(value)
             return assns + [ast.Expr(value_)]
-        case ast.If(test, body, orelse) if not isinstance(test, ast.Name):
+        case ast.Expr(ast.IfExp(test, body, orelse)):
+            return ast.If(test, [ast.Expr(body)], [ast.Expr(orelse)])
+        case ast.If(test, body, orelse) if not is_simple_value(test):
             fn = fvs.fresh()
             return [
                 ast.Assign([ast.Name(fn, ast.Store())], test),
                 ast.If(ast.Name(fn, ast.Load()), body, orelse)
             ]
-        case ast.Return(value) if value is not None and not isinstance(value, ast.Name):
+        case ast.Return(value) if value is not None and not is_simple_value(value):
             fn = fvs.fresh()
             return [
                 ast.Assign([ast.Name(fn, ast.Store())], value),
